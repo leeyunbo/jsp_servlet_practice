f4a5eb60ea00a6d2d07df3ab7836377b070e5501
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-01-16 13:56:39 +0900

    Spring 개발 설정
    말그대로 Spring Framework 개발 준비

---
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class SamsungTV {
+	public void powerOn() {
+		System.out.println("SamsungTV 전원키다.");
+	}
+	public void powerOff() {
+		System.out.println("SamsungTV 전원끄다.");
+	}
+	public void volumeUp() {
+		System.out.println("SamsungTV 소리 올리다.");
+	}
+	public void volumeDown() {
+		System.out.println("SamsungTV 소리 내리다.");
+	}
+}
